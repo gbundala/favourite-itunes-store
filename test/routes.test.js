40c4a99ec02cb8954bfa720e454106f49d609808
Gabriel Bundala <gabrielbundala@gmail.com>
2022-01-01 23:08:18 +0300

    setup and started implementation of backend unit test

---
+++ test/routes.test.js
@@ -0,0 +1,3 @@
+// Implement test that will test the api call being made in the routes including the fetch call
+// Also see how to expect something for this unit test as a result of the call being made
+// Research any other test you can do on the routes, server and any other areas in the backend
